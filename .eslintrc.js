d4a258611ab31ca63c745684886506fb2c0be5a6
leitingting08 <leitingting08@gmail.com>
2020-05-02 21:35:36 +0800

    chore: lint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jquery: true,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'off',
+      {
+        singleQuote: true,
+        semi: false,
+      },
+    ],
+    'vue/no-parsing-error': [
+      'off',
+      {
+        'x-invalid-end-tag': false,
+      },
+    ],
+  },
+  parserOptions: {
+    "ecmaVersion": 6,
+    parser: 'babel-eslint',
+  },
+  globals: {
+    AMap: true,
+  },
+}
