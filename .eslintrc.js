d7e13e6a6f6b999951da4898c08392f8ababe333
leitingting <leitingting@dxy.cn>
2020-05-10 00:34:03 +0800

    fix: 离开也显示消息

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': [
-      'off',
-      {
-        singleQuote: true,
-        semi: false,
-      },
-    ]
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     "ecmaVersion": 6,
